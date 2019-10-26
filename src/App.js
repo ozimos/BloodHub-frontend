98228d92d84e526294e6131dfadc86aed25938f4
Johnson Ojo <johnsonojo89@gmail.com>
2019-10-26 18:28:34 +0100

    add landing page

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ import SignUp from './components/SignUp'
 import './App.css';
 
 
-function Home() {
-  return (<div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
+function Home () {
+  return ( <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
     </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
     </a>
-  </header>
-</div>);
+    </header>
+  </div> );
 }
 
-function App() {
+function App () {
   return (
     <Router>
       <div>
@@ -61,6 +61,6 @@ function App() {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
